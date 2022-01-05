3baf516d26256f1ab4d1e514fcab08e7ce46f49b
ChanGenie <ckdwls0702@naver.com>
2022-01-05 17:07:06 +0900

    s3 off

--- team/app/src/main/java/com/meet/app/AppApplication.java
+++ team/app/src/main/java/com/meet/app/AppApplication.java
@@ -10,13 +10,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class AppApplication {
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.properties,"
-			+ "classpath:aws.yml";
+//	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+//			+ "classpath:application.properties,"
+//			+ "classpath:aws.yml";
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(AppApplication.class)
-				.properties(APPLICATION_LOCATIONS)
+//				.properties(APPLICATION_LOCATIONS)
 				.run(args);
 	}
 

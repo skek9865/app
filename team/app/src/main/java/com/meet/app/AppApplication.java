e5814bd88d9735c8da889706e07135ca868aaa71
skek <skek9865>
2022-01-06 14:28:27 +0900

    신고 관련

--- team/app/src/main/java/com/meet/app/AppApplication.java
+++ team/app/src/main/java/com/meet/app/AppApplication.java
@@ -11,8 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class AppApplication {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.properties,"
-			+ "classpath:aws.yml";
+			+ "classpath:application.properties";
+//			+ "classpath:aws.yml";
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(AppApplication.class)

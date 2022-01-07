9363c229583b716c4a4b230d7d3c7b170e2adaf5
skek9865 <83535254+skek9865@users.noreply.github.com>
2022-01-07 19:59:06 +0900

    Delete src/main/java/com/meet/app directory

--- src/main/java/com/meet/app/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.meet.app.repository;
-
-import com.meet.app.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
--- src/main/java/com/meet/app/repository/PrisonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.meet.app.repository;
-
-import com.meet.app.entity.Prison;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrisonRepository extends JpaRepository<Prison, Long> {
-}

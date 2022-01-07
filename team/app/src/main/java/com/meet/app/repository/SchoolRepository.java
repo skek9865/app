15a653a8698f2318577b43a6bf095ef2f7d206da
skek9865 <83535254+skek9865@users.noreply.github.com>
2022-01-07 19:59:16 +0900

    Delete team/app directory

--- team/app/src/main/java/com/meet/app/repository/SchoolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.meet.app.repository;
-
-import com.meet.app.entity.School;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolRepository extends JpaRepository<School,Integer> {
-}
--- team/app/src/main/java/com/meet/app/service/MemberInRoomService.java
+++
@@ -1,8 +0,0 @@
-package com.meet.app.service;
-
-public interface MemberInRoomService {
-
-    Boolean inRoom(Long roomID, String memberID);
-
-    Boolean outRoom(Long roomID, String memberID);
-}
